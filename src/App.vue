ac1d258b3fe3173346ded22eaa4a1308e09d0b5b
akash rattan <akashrattan43@Gmail.com>
2021-03-24 14:01:34 -0700

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+    <Modal /> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Modal from './components/Modal'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {Modal},
+  data() {
+    return {
+      title: "My first vue app",
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +28,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
